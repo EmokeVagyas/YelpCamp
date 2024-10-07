10fdf9d30fcb853ad9d878502eb4cf86c1f2ac8a
Emoke <emokevagyas@gmail.com>
2024-10-07 22:08:58 +0300

    Added flash messages for creating, updating, deleting campgrounds, handling "campground not found" errors, and for creating and deleting reviews.

--- models/campground.js
+++ models/campground.js
@@ -18,7 +18,7 @@ const CampgroundSchema = new Schema({
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
-        await Review.remove({
+        await Review.deleteMany({
             _id: {
                 $in: doc.reviews
             }
--- routes/reviews.js
+++ routes/reviews.js
@@ -26,6 +26,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
         campground.reviews.push(review);
         await review.save();
         await campground.save();
+        req.flash('success', 'Created new review!');
         res.redirect(`/campgrounds/${campground._id}`);
     } catch (e) {
         console.log(e);
@@ -36,6 +37,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Deleted review!');
     res.redirect(`/campgrounds/${id}`);
 }));
 
