b59b4a71ae7bfa7d350ac25582b63665089e901e
Emoke <emokevagyas@gmail.com>
2024-10-08 14:55:51 +0300

    Created User model with passport

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const { required } = require('joi');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', UserSchema)
