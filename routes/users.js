3a3916cf8044cc410cd14bad81e37569b8f0cc47
Emoke <emokevagyas@gmail.com>
2024-10-09 13:47:47 +0300

    Added logout

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const passport = require('passport');
+const { func } = require('joi');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -23,11 +24,21 @@ router.post('/register', catchAsync(async (req, res) => {
 
 router.get('/login', (req, res) => {
     res.render('users/login');
-})
+});
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome back!');
     res.redirect('/campgrounds');
-})
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
 
 module.exports = router;
