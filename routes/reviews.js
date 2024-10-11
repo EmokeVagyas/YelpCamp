33b45994820503d19a1f61080c2d1f1120f68b3c
Emoke <emokevagyas@gmail.com>
2024-10-11 12:23:21 +0300

    Ensure only the user can see and submit reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require('../middleware')
+const { validateReview, isLoggedIn } = require('../middleware')
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
@@ -8,10 +8,11 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 const { required } = require('joi');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', validateReview, isLoggedIn, catchAsync(async (req, res) => {
     try {
         const campground = await Campground.findById(req.params.id);
         const review = new Review(req.body.review);
+        review.author = req.user._id;
         campground.reviews.push(review);
         await review.save();
         await campground.save();
