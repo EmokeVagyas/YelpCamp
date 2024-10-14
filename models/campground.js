eedd045e433a39ffcf54ac35be2a552dd60bbdfa
Emoke <emokevagyas@gmail.com>
2024-10-14 13:18:38 +0300

    - Updated .gitignore - Added functionality so that when you edit campground, images will be displayed in smaller resolution

--- models/campground.js
+++ models/campground.js
@@ -3,15 +3,19 @@ const Review = require('./review');
 const { ref } = require('joi');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
     price: Number,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     description: String,
     location: String,
     author: {
