a6929fc1ea32ca41c810989800e0d1deb782bd17
Emoke <emokevagyas@gmail.com>
2024-10-17 15:13:13 +0300

    Added HTML sanitization using Joi extension for campground and review schemas to prevent injection attacks.

--- schemas.js
+++ schemas.js
@@ -1,6 +1,29 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
 const review = require('./models/review');
+const { sanitizeFilter } = require('mongoose');
+const sanitizeHtml = require('sanitize-html');
 
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean != value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 const campgroundSchema = Joi.object({
     campground: Joi.object({
@@ -16,7 +39,7 @@ const campgroundSchema = Joi.object({
 const reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
     }).required()
 })
 
